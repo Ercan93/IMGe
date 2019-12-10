0a9fe6f34639b1c85a9f14f2f8192a2976c2917e
Ercan Uzunsakal <ercan112358@gmail.com>
2019-12-10 23:59:33 +0300

    Process menu coded.

--- src/views/Preprocess.vue
+++ src/views/Preprocess.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <h1>Preprocess menu</h1>
-    <img :src="imageData" alt />
+  <div class="jumb">
+    <span class="badge badge-success head-text">PreProcess Menu</span>
+    <div class="jumbotron bg-info">
+      <img :src="imageData" width="300px" height="300px" alt />
+      <div class="process-buttons">
+        <button class="btn btn-success head-text">Birinci işlem</button>
+        <button class="btn btn-success head-text">Birinci işlem</button>
+        <button class="btn btn-success head-text">Birinci işlem</button>
+        <button class="btn btn-success head-text">Birinci işlem</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -17,3 +25,35 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.jumb {
+  margin-left: 200px;
+  margin-right: 400px;
+  margin-top: 50px;
+}
+.jumbotron {
+  margin-top: 24px;
+  height: 450px;
+  width: 600px;
+  display: flex;
+  justify-content: space-around;
+}
+.jumbotron img {
+  border: 3px solid black;
+}
+.head-text {
+  font-size: 25px;
+}
+span.head-text {
+  border: 2px solid black;
+}
+.process-buttons {
+  display: flex;
+  flex-direction: column;
+}
+.process-buttons button {
+  margin-bottom: 25px;
+  margin-left: 15px;
+}
+</style>
