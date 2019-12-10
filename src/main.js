26f390d86115dcfc6c970026ebce45c460a8b3f5
Ercan Uzunsakal <ercan112358@gmail.com>
2019-12-10 22:26:11 +0300

    Vue router created.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from "./store"
+import routes from "./routes"
+import VueRouter from "vue-router"
+
 import 'bootswatch/dist/sketchy/bootstrap.min.css';
+
+Vue.use(VueRouter);
+const router = new VueRouter({ mode: 'history', routes });
 
 new Vue({
         el: '#app',
         store,
+        routes,
         render: h => h(App)
 })
