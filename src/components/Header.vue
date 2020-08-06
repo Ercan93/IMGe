0249249a71417a241c225d98e271ffff93b0fca1
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-06 15:04:02 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,25 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark mt-4 mr-5 ml-5 bg-info">
-      <a class="navbar-brand" href="#">IMGe</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarColor01"
-        aria-controls="navbarColor01"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <router-link class="navbar-brand" tag="a" to="/">IMGe</router-link>
 
-      <div class="collapse navbar-collapse" id="navbarColor01">
-        <ul class="navbar-nav mr-auto">
-          <router-link class="nav-link" tag="li" to="/features">Özellikler</router-link>
-          <router-link class="nav-link" tag="li" to="/about">Hakkında</router-link>
-        </ul>
+      <div class="navbar-nav mr-auto">
+        <router-link class="nav-link" tag="a" to="/features">Özellikler</router-link>
+        <router-link class="nav-link" tag="a" to="/about">Hakkında</router-link>
       </div>
-      <button
-        v-if="$route.name != 'file-chooser'"
-        class="btn btn-warning border-dark text-dark mr-3"
-        @click="PreviousFunction"
-      >Geri</button>
-      <button
-        v-if="$route.name != 'segmentation-menu'"
-        class="btn btn-warning border-dark font-weight-bold text-dark mr-5"
-        @click="nextFunction"
-      >İleri</button>
+
+      <div class="ml-auto">
+        <button
+          v-if="$route.name != 'file-chooser'"
+          class="btn btn-warning border-dark text-dark"
+          @click="PreviousFunction"
+        >Previous</button>
+        <button
+          v-if="$route.name != 'segmentation-menu'"
+          class="btn btn-warning border-dark font-weight-bold text-dark mr-5"
+          @click="nextFunction"
+        >Next</button>
+      </div>
     </nav>
   </div>
 </template>
